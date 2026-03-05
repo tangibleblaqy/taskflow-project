864d1a4c20f987813c3d323e07bdbc17e61a7bc1
Yixiao <zhanyixiao06@gmail.com>
2026-03-05 22:09:51 +0100

    segundo ejercicio, html completado, js falta funcion de borrar tareas y la busqueda de tareas

---
+++ FCT_2/app.js
@@ -0,0 +1,40 @@
+const tareasGuardadas = [];
+let tareaActual;
+
+const boton = document.getElementById("boton");
+const buscador = document.getElementById("buscador");
+const creador = document.getElementById("creador");
+const tareas = document.getElementById("tareas");
+
+const borrarFormulario = () => {
+    creador.value = "";
+}
+
+const crearTarea = (tareaActual, tareas) => {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    const button = document.createElement("button");
+
+    p.textContent = tareaActual + "\u00A0";
+    button.textContent = "Borrar";
+    div.classList.add(tareaActual);
+
+    div.append(p, button);
+    tareas.append(div);
+}
+
+boton.addEventListener("click", (e) =>{
+    e.preventDefault();
+    tareaActual = creador.value.trim();
+
+    if (tareaActual === "") {
+    borrarFormulario();
+    return alert("La tarea no puede estar vacío");
+    } else if(tareasGuardadas.some((value) => value.toLowerCase() === tareaActual.toLowerCase())){
+    borrarFormulario();
+    return alert("No puede haber tareas repetidas"); 
+    }
+    tareasGuardadas.push(tareaActual);
+    crearTarea(tareaActual, tareas);
+    borrarFormulario();
+})
